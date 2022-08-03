2fdd453d4a912dd345b4312ffe8a8ad7fea73a7e
MaddinaSruthi <106723526+MaddinaSruthi@users.noreply.github.com>
2022-08-03 15:49:06 +0530

    Initial commit

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+import "./landing.css";
+import startup from "../../static/images/startup.svg";
+
+
+function Landing() {
+  return (
+    <div className="landing">
+      <div className="landingWrapper">
+        <section className="landingContainer">
+          <header className="landingHeader">Progress Inspector</header>
+          <span className="landingText"> - A Project Management Tool</span>
+        </section>
+        <aside className="landingImage">
+          <img src={startup} alt="Landing Page" className="landingStartup" />
+        </aside>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
